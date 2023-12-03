e688b9c3d059b674ed05d1433cb4ef22412ea31f
zakiaaminia <152748628+zakiaaminia@users.noreply.github.com>
2023-12-03 10:48:30 +0700

    Add files via upload

---
+++ public/assets/js/custom.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * You can write your JS code here, DO NOT touch the default style file
+ * because it will make it harder for you to update.
+ * 
+ */
+
+"use strict";
+
+$('.daterange-cus').daterangepicker({
+    locale: {format: 'DD-MM-YYYY'},
+    drops: 'down',
+    opens: 'right'
+});
